93d6a71102153540ba2e4fd1996711fced7e0f39
ghshin <leteu610@gmail.com>
2022-06-09 22:53:38 +0900

    통계 명령어 추가

---
+++ src/commands/statistics.ts
@@ -0,0 +1,40 @@
+import { graphql } from "@octokit/graphql";
+import { MessageEmbed } from "discord.js";
+import type { SimpleCommandMessage } from "discordx";
+import { Discord, SimpleCommand } from "discordx";
+
+const accessToken = process.env.GITHUB_TOKEN;
+const githubGraphqlAPI = graphql.defaults({
+  headers: {
+    Authorization: `Bearer ${accessToken || ""}`,
+  }
+});
+
+const statQuery = ($username: string) => `
+{
+  user(login: "${$username}"){
+    contributionsCollection {
+      contributionCalendar {
+        totalContributions
+        
+      }
+    }
+  }
+}
+`;
+
+const statisticsEmbed = new MessageEmbed()
+    .setColor("GREEN")
+
+@Discord()
+export class Statistics {
+  @SimpleCommand("stat", { aliases: ["통계"] })
+  async SimplePing(command: SimpleCommandMessage): Promise<void> {
+    const $username = command.message.content.replace("!stat ", "").replace("!통계 ", '').trim().split(/ +/g);
+
+    $username.forEach(async arg => {
+      const { user } = await githubGraphqlAPI(statQuery(arg));
+      console.log(user.contributionsCollection);
+    })
+  }
+}
