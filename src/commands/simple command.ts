c9f8712b1f6068d0b1ba6f8dfe86dcc0606af5c8
ghshin <leteu610@gmail.com>
2022-06-09 14:09:08 +0900

    핑퐁 추가

--- src/commands/simple command.ts
+++ src/commands/simple command.ts
@@ -1,14 +1,15 @@
-import { CommandInteraction, Interaction, Message } from "discord.js";
+import { CommandInteraction, Message } from "discord.js";
 import type { SimpleCommandMessage } from "discordx";
 import {
   Discord,
   SimpleCommand,
-  SimpleCommandOption,
-  SimpleCommandOptionType,
   Slash,
 } from "discordx";
 
 @Discord()
 export class Example {
-  
+  @SimpleCommand('ping')
+  SimplePing(command: SimpleCommandMessage): void {
+    command.message.reply('pong!! 🏓')
+  }
 }
