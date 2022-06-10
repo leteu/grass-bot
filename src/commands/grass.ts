77b35c3914c9e8d040a4537e2b4428437734bfa0
ghshin <leteu610@gmail.com>
2022-06-10 16:39:41 +0900

    기여도 데이터 가공 및 데이터 인터페이스 작성

--- src/commands/grass.ts
+++ src/commands/grass.ts
@@ -1,12 +1,52 @@
+import util from "util";
 import { graphql } from "@octokit/graphql";
 import type { SimpleCommandMessage } from "discordx";
 import { Discord, SimpleCommand } from "discordx";
+import grassWidget from '../grassWidget';
+import { MessageEmbed } from "discord.js";
+
+type HexColorString = `#${string}`;
+type ContributionLevelType =
+  | "NONE"
+  | `${"FIRST" | "SECOND" | "THIRD" | "FOURTH"}_QUARTILE`;
+type weekdayType = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+export interface ContributionDay {
+  contributionCount: number;
+  /**
+   * #000000 ~ #FFFFFF
+   */
+  color: HexColorString;
+  /**
+   * Date - An ISO-8601 encoded date string.
+   */
+  date: string;
+  /**
+   * **NONE** - *No contributions occurred.*   
+   * **FIRST_QUARTILE** - *Lowest 25% of days of contributions.*   
+   * **SECOND_QUARTILE** - *Second lowest 25% of days of contributions. More contributions than the first quartile.*   
+   * **THIRD_QUARTILE** - *Second highest 25% of days of contributions. More contributions than second quartile, less than the fourth quartile.*   
+   * **FOURTH_QUARTILE** - *Highest 25% of days of contributions. More contributions than the third quartile.*
+   */
+  contributionLevel: ContributionLevelType;
+  /**
+   * number   
+   * **0** - *Sunday(일)*   
+   * **1** - *Monday(월)*   
+   * **2** - *Tuesday(화)*   
+   * **3** - *Wednesday(수)*   
+   * **4** - *Thursday(목)*   
+   * **5** - *Friday(금)*   
+   * **6** - *Saturday(토)*   
+   */
+  weekday: weekdayType;
+}
 
 const accessToken = process.env.GITHUB_TOKEN;
 const githubGraphqlAPI = graphql.defaults({
   headers: {
     Authorization: `Bearer ${accessToken || ""}`,
-  }
+  },
 });
 
 const DateQuery = (username: string) => `
@@ -29,15 +69,47 @@ const DateQuery = (username: string) => `
 }
 `;
 
+function divistion(arr: ContributionDay[], n: number) {
+  const len = arr.length;
+  const cnt = Math.floor(len / n) + (Math.floor(len % n) > 0 ? 1 : 0);
+  let tmp = [];
+
+  for (var i = 0; i < cnt; i++) {
+    tmp.push(arr.splice(0, n));
+  }
+
+  return tmp;
+}
+
+
+
 @Discord()
 export class Grass {
   @SimpleCommand("grass", { aliases: ["잔디"] })
   async SimplePing(command: SimpleCommandMessage): Promise<void> {
-    const username = command.message.content.replace("!잔디 ", "").replace("!grass ", '').trim().split(/ +/g);
+    command
 
-    username.forEach(async arg => {
+    const username = command.message.content
+      .replace("!", "").trim()
+      .replace("잔디 ", "").trim()
+      .replace("grass ", "").trim()
+      .split(/ +/g);
+
+    if (username.length === 0) return;
+
+    username.forEach(async (arg) => {
       const { user } = await githubGraphqlAPI(DateQuery(arg));
-      console.log(user.contributionsCollection);
-    })
+      const weeks: { contributionDays: ContributionDay[] }[] =
+        user.contributionsCollection.contributionCalendar.weeks;
+
+      const dateArr = divistion(weeks
+        .reverse()
+        .slice(0, 15)
+        .reverse()
+        .map((item) => item.contributionDays)
+        .flat(2), 7);
+
+      console.log(util.inspect(dateArr, false, 3));
+    });
   }
 }
