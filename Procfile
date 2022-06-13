9c2756eb41be8732ab40de3b0fd047694fbc48c7
ghshin <leteu610@gmail.com>
2022-06-13 17:07:38 +0900

    fix config

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: export BOT_TOKEN=process.env.BOT_TOKEN && npm run start
+worker: npm run start
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    BOT_TOKEN: string;
     GITHUB_API_TOKEN: string;
   }
 }
