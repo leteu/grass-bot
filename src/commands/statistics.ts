3bd321c60876eeb16c5bef65dbe0b784c40c2bad
ghshin <leteu610@gmail.com>
2022-12-18 23:53:38 +0900

    Feat: 통계 명령어 슬래시 지원

--- src/commands/statistics.ts
+++ src/commands/statistics.ts
@@ -1,14 +1,14 @@
-import { graphql } from "@octokit/graphql";
-import { MessageEmbed } from "discord.js";
-import type { SimpleCommandMessage } from "discordx";
-import { Discord, SimpleCommand } from "discordx";
+import { graphql } from '@octokit/graphql'
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+import { Discord, Slash, SlashOption, SimpleCommand, SimpleCommandMessage } from 'discordx'
+import { GithubGraphQL } from 'types'
 
-const accessToken = process.env.GITHUB_TOKEN;
+const accessToken = process.env.GITHUB_TOKEN
 const githubGraphqlAPI = graphql.defaults({
   headers: {
-    Authorization: `Bearer ${accessToken || ""}`,
+    Authorization: `Bearer ${accessToken || ''}`,
   },
-});
+})
 
 const statQuery = (username: string) => `
 {
@@ -26,53 +26,74 @@ const statQuery = (username: string) => `
     }
   }
 }
-`;
+`
 
-const statisticsEmbed = new MessageEmbed().setColor("GREEN");
+const statisticsEmbed = new MessageEmbed().setColor('GREEN')
 
 @Discord()
 export class Statistics {
-  @SimpleCommand("stats", { aliases: ["통계"] })
+  @SimpleCommand('stats', { aliases: ['통계'] })
   async SimplePing(command: SimpleCommandMessage): Promise<void> {
     const username = command.message.content
-      .replace("!", "")
+      .replace('!', '')
       .trim()
-      .replace("통계 ", "")
+      .replace('통계 ', '')
       .trim()
-      .replace("stats ", "")
+      .replace('stats ', '')
       .trim()
-      .split(/ +/g);
+      .split(/ +/g)
 
-    if (username.length === 0) throw void 0;
+    if (username.length === 0) throw void 0
 
     username.forEach(async (arg) => {
-      try {
-        const { user } = await githubGraphqlAPI(statQuery(arg));
+      const embed = await this.getStatsEmbed(arg)
 
-        const toContributions = user.contributionsCollection.contributionCalendar.totalContributions;
-        const toIssues = user.contributionsCollection.issueContributions.totalCount;
-        const toPRs = user.contributionsCollection.pullRequestContributions.totalCount;
+      await command.message.reply(embed)
+    })
+  }
 
-        const embed = new MessageEmbed()
-          .setColor("AQUA")
-          .setTitle(`${arg}'s Github Statistics`)
-          .setAuthor({ name: 'leteu', iconURL: 'https://avatars.githubusercontent.com/u/77822996?v=4', url: 'https://github.com/leteu' })
-          .setTimestamp()
-          .setFooter({ text: '문의 : leteu#0718' })
-          .addFields(
-            { name: 'Total contributions', value: `${toContributions}` },
-            { name: 'Total issues', value: `${toIssues}` },
-            { name: 'Total pull requests', value: `${toPRs}` },
-          );
+  @Slash('stats')
+  @Slash('통계')
+  async SlashStats(
+    @SlashOption('username', { description: 'Github username', required: true })
+    username: string,
 
-        command.message.reply({ embeds: [embed] });
-      } catch (e) {
-        console.log(e);
+    interaction: CommandInteraction
+  ): Promise<void> {
+    const embed = await this.getStatsEmbed(username)
 
-        command.message.reply(
-          `${arg} is not defined.\n${arg}를 찾을 수 없습니다.`
-        );
-      }
-    });
+    await interaction.reply(embed)
+  }
+
+  public async getStatsEmbed(username: string) {
+    try {
+      const { user } = await githubGraphqlAPI<GithubGraphQL>(statQuery(username))
+
+      const toContributions = user.contributionsCollection.contributionCalendar.totalContributions
+      const toIssues = user.contributionsCollection.issueContributions.totalCount
+      const toPRs = user.contributionsCollection.pullRequestContributions.totalCount
+
+      const embed = new MessageEmbed()
+        .setColor('AQUA')
+        .setTitle(`${username}'s Github Statistics`)
+        .setAuthor({
+          name: 'leteu',
+          iconURL: 'https://avatars.githubusercontent.com/u/77822996?v=4',
+          url: 'https://github.com/leteu',
+        })
+        .setTimestamp()
+        .setFooter({ text: '문의 : leteu#0718' })
+        .addFields(
+          { name: 'Total contributions', value: `${toContributions}` },
+          { name: 'Total issues', value: `${toIssues}` },
+          { name: 'Total pull requests', value: `${toPRs}` }
+        )
+
+      return { embeds: [embed] }
+    } catch (e) {
+      console.log(e)
+
+      return `${username} is not defined.\n${username}를 찾을 수 없습니다.`
+    }
   }
 }
