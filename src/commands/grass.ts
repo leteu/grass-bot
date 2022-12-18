db5a253b4a616828c740b61ab08627a7a215dec5
ghshin <leteu610@gmail.com>
2022-12-18 23:54:13 +0900

    Fix: 잔디 명령어 코드 정리 및 슬래시 명령어 준비

--- src/commands/grass.ts
+++ src/commands/grass.ts
@@ -1,55 +1,19 @@
-import { graphql } from "@octokit/graphql";
-import type { SimpleCommandMessage } from "discordx";
-import { Discord, SimpleCommand } from "discordx";
-import { grassWidget } from "../events/grassWidget";
-import { MessageAttachment, MessageEmbed } from "discord.js";
-import * as path from "path";
+import { graphql } from '@octokit/graphql'
+import { SimpleCommandMessage, Slash, SlashOption } from 'discordx'
+import { Discord, SimpleCommand } from 'discordx'
+import { grassWidget } from '../events/grassWidget'
+import { CommandInteraction, MessageAttachment, MessageEmbed } from 'discord.js'
+import * as path from 'path'
 // const __dirname = path.resolve();
-import * as fs from "fs";
+import * as fs from 'fs'
+import { ContributionDay, GithubGraphQL } from 'types'
 
-type HexColorString = `#${string}`;
-type ContributionLevelType =
-  | "NONE"
-  | `${"FIRST" | "SECOND" | "THIRD" | "FOURTH"}_QUARTILE`;
-type weekdayType = 0 | 1 | 2 | 3 | 4 | 5 | 6;
-
-export interface ContributionDay {
-  contributionCount: number;
-  /**
-   * #000000 ~ #FFFFFF
-   */
-  color: HexColorString;
-  /**
-   * Date - An ISO-8601 encoded date string.
-   */
-  date: string;
-  /**
-   * **NONE** - *No contributions occurred.*
-   * **FIRST_QUARTILE** - *Lowest 25% of days of contributions.*
-   * **SECOND_QUARTILE** - *Second lowest 25% of days of contributions. More contributions than the first quartile.*
-   * **THIRD_QUARTILE** - *Second highest 25% of days of contributions. More contributions than second quartile, less than the fourth quartile.*
-   * **FOURTH_QUARTILE** - *Highest 25% of days of contributions. More contributions than the third quartile.*
-   */
-  contributionLevel: ContributionLevelType;
-  /**
-   * number
-   * **0** - *Sunday(일)*
-   * **1** - *Monday(월)*
-   * **2** - *Tuesday(화)*
-   * **3** - *Wednesday(수)*
-   * **4** - *Thursday(목)*
-   * **5** - *Friday(금)*
-   * **6** - *Saturday(토)*
-   */
-  weekday: weekdayType;
-}
-
-const accessToken = process.env.GITHUB_TOKEN;
+const accessToken = process.env.GITHUB_TOKEN
 const githubGraphqlAPI = graphql.defaults({
   headers: {
-    Authorization: `Bearer ${accessToken || ""}`,
+    Authorization: `Bearer ${accessToken || ''}`,
   },
-});
+})
 
 const DateQuery = (username: string) => `
 {
@@ -70,98 +34,129 @@ const DateQuery = (username: string) => `
     }
   }
 }
-`;
+`
 
 function divistion(arr: ContributionDay[], n: number) {
-  const len = arr.length;
-  const cnt = Math.floor(len / n) + (Math.floor(len % n) > 0 ? 1 : 0);
-  let tmp = [];
+  const len = arr.length
+  const cnt = Math.floor(len / n) + (Math.floor(len % n) > 0 ? 1 : 0)
+  let tmp = []
 
   for (var i = 0; i < cnt; i++) {
-    tmp.push(arr.splice(0, n));
+    tmp.push(arr.splice(0, n))
   }
 
-  return tmp;
+  return tmp
 }
 
 @Discord()
 export class Grass {
-  @SimpleCommand("grass", { aliases: ["잔디"] })
-  async SimplePing(command: SimpleCommandMessage): Promise<void> {
-    command;
+  @SimpleCommand('grass', { aliases: ['잔디'] })
+  async SimpleGrass(command: SimpleCommandMessage): Promise<void> {
+    command
 
     const username = command.message.content
-      .replace("!", "")
+      .replace('!', '')
       .trim()
-      .replace("잔디 ", "")
+      .replace('잔디 ', '')
       .trim()
-      .replace("grass ", "")
+      .replace('grass ', '')
       .trim()
-      .split(/ +/g);
+      .split(/ +/g)
 
-    if (username.length === 0) throw void 0;
+    if (username.length === 0) throw void 0
 
     username.forEach(async (arg) => {
-      try {
-        const { user } = await githubGraphqlAPI(DateQuery(arg));
-        const weeks: { contributionDays: ContributionDay[] }[] =
-          user.contributionsCollection.contributionCalendar.weeks;
-        const total =
-          user.contributionsCollection.contributionCalendar.totalContributions;
+      const result = await this.getGrassEmbed(arg)
 
-        const dateArr = divistion(
-          weeks
-            .reverse()
-            .slice(0, 16)
-            .reverse()
-            .map((item) => item.contributionDays)
-            .flat(2),
-          7
-        );
+      if (typeof result === 'string') {
+        command.message.reply(result)
+      } else {
+        await command.message.reply({
+          embeds: [result.embed],
+          files: [result.file],
+        })
 
-        await grassWidget(arg, total, dateArr);
-
-        const file = new MessageAttachment(
-          path.resolve(path.resolve(), "users", `${arg}.png`)
-        );
-        const embed = new MessageEmbed()
-          .setColor("AQUA")
-          .setTitle(`${arg}'s 잔디`)
-          .setDescription("Github contributions in the last 16 weeks")
-          .setImage(`attachment://${arg}.png`)
-          .setAuthor({ name: "leteu", url: "https://github.com/leteu" })
-          .setTimestamp()
-          .setFooter({
-            text: "문의 : leteu#0718",
-            iconURL: "https://avatars.githubusercontent.com/u/77822996?v=4",
-          });
-
-        command.message
-          .reply({
-            embeds: [embed],
-            files: [file],
-          })
-          .then(() => {
-            fs.unlink(
-              path.resolve(path.resolve(), "users", `${arg}.png`),
-              function (err) {
-                if (err) throw err;
-                console.log(`successfully deleted ${arg}.png`);
-              }
-            );
-            fs.unlink(
-              path.resolve(path.resolve(), "users", `${arg}.svg`),
-              function (err) {
-                if (err) throw err;
-                console.log(`successfully deleted ${arg}.svg`);
-              }
-            );
-          });
-      } catch (e) {
-        command.message.reply(
-          `${arg} is not defined.\n${arg}를 찾을 수 없습니다.`
-        );
+        fs.unlink(path.resolve(path.resolve(), 'users', `${arg}.png`), function (err) {
+          if (err) throw err
+          console.log(`successfully deleted ${arg}.png`)
+        })
+        fs.unlink(path.resolve(path.resolve(), 'users', `${arg}.svg`), function (err) {
+          if (err) throw err
+          console.log(`successfully deleted ${arg}.svg`)
+        })
       }
-    });
+    })
+  }
+
+  @Slash('grass')
+  @Slash('잔디')
+  async SlashGrass(
+    @SlashOption('username', { description: 'Github username', required: true })
+    username: string,
+
+    interaction: CommandInteraction
+  ): Promise<void> {
+    interaction.reply({
+      content:
+        'slash grass command will be released soon...\nplease use `!grass <username>` instead of splash\n\n슬래시 잔디 명령어는 준비중입니다...\n슬래시 명령어 대신 `!잔디 <username>`을 사용해보세요!',
+      ephemeral: true,
+    })
+
+    // const result = await this.getGrassEmbed(username)
+
+    // if (typeof result === 'string') {
+    //   interaction.reply(result)
+    // } else {
+    //   await interaction.reply({
+    //     embeds: [result.embed],
+    //     files: [result.file],
+    //   })
+
+    //   fs.unlink(path.resolve(path.resolve(), 'users', `${username}.png`), function (err) {
+    //     if (err) throw err
+    //     console.log(`successfully deleted ${username}.png`)
+    //   })
+    //   fs.unlink(path.resolve(path.resolve(), 'users', `${username}.svg`), function (err) {
+    //     if (err) throw err
+    //     console.log(`successfully deleted ${username}.svg`)
+    //   })
+    // }
+  }
+
+  public async getGrassEmbed(username: string) {
+    try {
+      const { user } = await githubGraphqlAPI<GithubGraphQL>(DateQuery(username))
+      const weeks = user.contributionsCollection.contributionCalendar.weeks
+      const total = user.contributionsCollection.contributionCalendar.totalContributions
+
+      const dateArr = divistion(
+        weeks
+          .reverse()
+          .slice(0, 16)
+          .reverse()
+          .map((item) => item.contributionDays)
+          .flat(2),
+        7
+      )
+
+      await grassWidget(username, total, dateArr)
+
+      const file = new MessageAttachment(path.resolve(path.resolve(), 'users', `${username}.png`))
+      const embed = new MessageEmbed()
+        .setColor('AQUA')
+        .setTitle(`${username}'s 잔디`)
+        .setDescription('Github contributions in the last 16 weeks')
+        .setImage(`attachment://${username}.png`)
+        .setAuthor({ name: 'leteu', url: 'https://github.com/leteu' })
+        .setTimestamp()
+        .setFooter({
+          text: '문의 : leteu#0718',
+          iconURL: 'https://avatars.githubusercontent.com/u/77822996?v=4',
+        })
+
+      return { embed, file }
+    } catch (e) {
+      return `${username} is not defined.\n${username}를 찾을 수 없습니다.`
+    }
   }
 }
